d3bafc1834a127b5408bb944387f11c6db5d873e
Gustavo <gustavopuentefranco@dipusevilla.es>
2024-02-22 20:18:43 +0100

    Final ejercicio 1

---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+export function TwitterFollowCard({formatUserName, userName, name, isFollowing}) {
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          alt="El avatar de gulas"
+          src={`https://unavatar.io/${userName}`}
+          
+        />
+        <div className="tw-followCard-info">
+          <strong>{name}</strong>
+          <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>    </article>
+  );
+}
